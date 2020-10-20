b91e0f603105eebcfdd06e50ecafe5ed539c6e6e
Anmol <mr.anmolthapar@gmail.com>
2020-10-21 11:46:16 +1300

    Student methods updated

--- src/com/softeng306/StudentMgr.java
+++ src/com/softeng306/StudentMgr.java
@@ -17,12 +17,12 @@ public class StudentMgr {
      * Adds a student and put the student into file
      */
     public static void addStudent() {
-        String studentName, studentSchool;
-        String studentID = null;
-        int choice, studentYear;
-        boolean studentExists;
+        String studentName;
+        String studentID;
+        int choice ;
+
         String GPA = "not available";
-        Student currentStudent = null;
+        Student currentStudent;
         System.out.println("addStudent is called");
         System.out.println("Choose the way you want to add a student:");
         System.out.println("1. Manually input the student ID.");
@@ -42,38 +42,87 @@ public class StudentMgr {
             }
         } while (true);
         if (choice == 1) {
-            while (true) {
-                System.out.println("The student ID should follow:");
-                System.out.println("Length is exactly 9");
-                System.out.println("Start with U (Undergraduate)");
-                System.out.println("End with a uppercase letter between A and L");
-                System.out.println("Seven digits in the middle");
-                System.out.println();
-                System.out.println("Give this student an ID: ");
-                studentID = scanner.nextLine();
-                if (ValidationMgr.checkValidStudentIDInput(studentID)) {
-                    if (ValidationMgr.checkStudentExists(studentID) == null) {
-                        break;
-                    }
-                }
-            }
+            do {
+//                prompt user for with instructions for manual ID input
+                studentID = manualPromptUserID();
+//                if valid ID and student does not exists then break
+            } while (!ValidationMgr.checkValidStudentIDInput(studentID) || ValidationMgr.checkStudentExists(studentID) != null);
         }
-
-        while (true) {
+//      if choice ==2 then generate automated student ID
+        else {
+            studentID = generateStudentID();
+        }
+//
+        do {
             System.out.println("Enter student Name: ");
             studentName = scanner.nextLine();
-            if (ValidationMgr.checkValidPersonNameInput(studentName)) {
-                break;
+        } while (!ValidationMgr.checkValidPersonNameInput(studentName));
+//        create new student with name and student ID
+        currentStudent = new Student(studentID,studentName);
+
+        //set student department
+        setStudentDepartment(currentStudent);
+        //set gender
+        setStudentGender(currentStudent);
+
+        //student year
+        setStudentYear(currentStudent);
+//        write into file
+        FILEMgr.writeStudentsIntoFile(currentStudent);
+
+        Main.students.add(currentStudent);
+        System.out.println("Student named: " + studentName + " is added, with ID: " + currentStudent.getStudentID());
+
+        System.out.println("Student List: ");
+        System.out.println("| Student ID | Student Name | Student School | Gender | Year | GPA |");
+        for (Student student : Main.students) {
+            if (Double.compare(student.getGPA(), 0.0) != 0) {
+                GPA = String.valueOf(student.getGPA());
             }
+            System.out.println(" " + student.getStudentID() + " | " + student.getStudentName() + " | " + student.getStudentSchool() + " | " + student.getGender() + " | " + student.getStudentYear() + " | " + GPA);
         }
 
-        currentStudent = new Student(studentName);
-        if (choice == 1) {
-            currentStudent.setStudentID(studentID);
-        }
+    }
+    /**
+     * Generates the ID of a new student.
+     * @return the generated student ID.
+     */
+    private static String generateStudentID() {
+        String generateStudentID;
+        boolean studentIDUsed;
+        int idNumber = 1800000;
+        do {
+            int rand = (int) (Math.random() * ((76 - 65) + 1)) + 65;
+            String lastPlace = Character.toString((char) rand);
+            idNumber += 1;
+            generateStudentID = "U" + idNumber + lastPlace;
+            studentIDUsed = false;
+            for (Student student : Main.students) {
+                if (generateStudentID.equals(student.getStudentID())) {
+                    studentIDUsed = true;
+                    break;
+                }
+            }
+        } while (studentIDUsed);
+        return generateStudentID;
+    }
 
-
-        //student department
+    /**
+     * IF User enters student ID manually, this is the prompt given
+     * @return the user inputted Student ID
+     */
+    private static String manualPromptUserID(){
+        System.out.println("The student ID should follow:");
+        System.out.println("Length is exactly 9");
+        System.out.println("Start with U (Undergraduate)");
+        System.out.println("End with a uppercase letter between A and L");
+        System.out.println("Seven digits in the middle");
+        System.out.println();
+        System.out.println("Give this student an ID: ");
+        return scanner.nextLine();
+    }
+    private static void setStudentDepartment(Student currentStudent){
+        String studentSchool;
         while (true) {
             System.out.println("Enter student's school (uppercase): ");
             System.out.println("Enter -h to print all the schools.");
@@ -82,15 +131,13 @@ public class StudentMgr {
                 HelpInfoMgr.printAllDepartment();
                 studentSchool = scanner.nextLine();
             }
-
             if (ValidationMgr.checkDepartmentValidation(studentSchool)) {
                 currentStudent.setStudentSchool(studentSchool);
                 break;
             }
         }
-
-
-        //gender
+    }
+    private static void setStudentGender(Student currentStudent) {
         String studentGender;
         while (true) {
             System.out.println("Enter student gender (uppercase): ");
@@ -106,9 +153,9 @@ public class StudentMgr {
                 break;
             }
         }
-
-
-        //student year
+    }
+    private static void setStudentYear(Student currentStudent) {
+        int studentYear;
         do {
             System.out.println("Enter student's school year (1-4) : ");
             if (scanner.hasNextInt()) {
@@ -126,21 +173,5 @@ public class StudentMgr {
                 System.out.println("Please re-enter.");
             }
         } while (true);
-
-
-        FILEMgr.writeStudentsIntoFile(currentStudent);
-
-        Main.students.add(currentStudent);
-        System.out.println("Student named: " + studentName + " is added, with ID: " + currentStudent.getStudentID());
-
-        System.out.println("Student List: ");
-        System.out.println("| Student ID | Student Name | Student School | Gender | Year | GPA |");
-        for (Student student : Main.students) {
-            if (Double.compare(student.getGPA(), 0.0) != 0) {
-                GPA = String.valueOf(student.getGPA());
-            }
-            System.out.println(" " + student.getStudentID() + " | " + student.getStudentName() + " | " + student.getStudentSchool() + " | " + student.getGender() + " | " + student.getStudentYear() + " | " + GPA);
-        }
-
     }
 }
