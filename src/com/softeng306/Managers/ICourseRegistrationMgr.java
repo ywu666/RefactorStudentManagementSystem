28c4c29c7eec808d51f8717e6b554d2bd77a27bd
Callum <cdan879@aucklanduni.ac.nz>
2020-10-21 11:09:06 +1300

    Create CourseMgr Interface. Change return type of checkCourseRegistrationExists method in ICourseRegistrationMgr from CourseRegistration to boolean

--- src/com/softeng306/Managers/ICourseRegistrationMgr.java
+++ src/com/softeng306/Managers/ICourseRegistrationMgr.java
@@ -1,13 +1,11 @@
 package com.softeng306.Managers;
 
-import com.softeng306.CourseRegistration;
-
 public interface ICourseRegistrationMgr {
 
     void registerCourse();
 
     void printStudents();
 
-    CourseRegistration checkCourseRegistrationExists();
+    boolean checkCourseRegistrationExists();
 
 }
