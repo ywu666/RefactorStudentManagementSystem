e0866e8236a740764d9a906c4e7b4f66e4af7b34
xinyi <xguo679@aucklanduni.ac.nz>
2020-10-23 02:33:09 +1300

    refactor setCourseWorkMark() function

--- src/com/softeng306/MarkMgr.java
+++ src/com/softeng306/MarkMgr.java
@@ -73,71 +73,73 @@ public class MarkMgr {
 
             } else {
                 System.out.println("Here are the choices you can have: ");
-                ArrayList<String> availableChoices = new ArrayList<String>(0);
-                ArrayList<Double> weights = new ArrayList<Double>(0);
-                ArrayList<Boolean> isMainAss = new ArrayList<Boolean>(0);
-                for (HashMap.Entry<CourseworkComponent, Double> assessmentResult : mark.getCourseWorkMarks().entrySet()) {
-                    CourseworkComponent key = assessmentResult.getKey();
-                    if (key instanceof MainComponent) {
-                        if ((!key.getComponentName().equals("Exam")) && ((MainComponent) key).getSubComponents().size() == 0) {
-                            availableChoices.add(key.getComponentName());
-                            weights.add((double) key.getComponentWeight());
-                            isMainAss.add(true);
-                        } else {
-                            for (SubComponent subComponent : ((MainComponent) key).getSubComponents()) {
-                                availableChoices.add(key.getComponentName() + "-" + subComponent.getComponentName());
-                                weights.add((double) key.getComponentWeight() * (double) subComponent.getComponentWeight() / 100d);
-                                isMainAss.add(false);
-                            }
-                        }
-                    }
-                }
+                printChoicesForCourseWorkMark(mark);
+            }
+            return;
+        }
+        System.out.println("This student haven't registered " + courseID);
+    }
 
-                for (int i = 0; i < availableChoices.size(); i++) {
-                    System.out.println((i + 1) + ". " + availableChoices.get(i) + " Weight in Total: " + weights.get(i) + "%");
-                }
-                System.out.println((availableChoices.size() + 1) + ". Quit");
-
-                int choice;
-                System.out.println("Enter your choice");
-                choice = scanner.nextInt();
-                scanner.nextLine();
-
-                while (choice > (availableChoices.size() + 1) || choice < 0) {
-                    System.out.println("Please enter choice between " + 0 + "~" + (availableChoices.size() + 1));
-                    System.out.println("Enter your choice");
-                    choice = scanner.nextInt();
-                    scanner.nextLine();
-                }
-
-                if (choice == (availableChoices.size() + 1)) {
-                    return;
-                }
-
-                double assessmentMark;
-                System.out.println("Enter the mark for this assessment:");
-                assessmentMark = scanner.nextDouble();
-                scanner.nextLine();
-                while (assessmentMark > 100 || assessmentMark < 0) {
-                    System.out.println("Please enter mark in range 0 ~ 100.");
-                    assessmentMark = scanner.nextDouble();
-                    scanner.nextLine();
-                }
-
-                if (isMainAss.get(choice - 1)) {
-                    // This is a stand alone main assessment
-                    mark.setMainCourseWorkMarks(availableChoices.get(choice - 1), assessmentMark);
-                } else {
-                    mark.setSubCourseWorkMarks(availableChoices.get(choice - 1).split("-")[1], assessmentMark);
-                }
-
+    public static void printChoicesForCourseWorkMark(Mark mark){
+        ArrayList<String> availableChoices = new ArrayList<String>(0);
+        ArrayList<Double> weights = new ArrayList<Double>(0);
+        ArrayList<Boolean> isMainAss = new ArrayList<Boolean>(0);
+        for (HashMap.Entry<CourseworkComponent, Double> assessmentResult : mark.getCourseWorkMarks().entrySet()) {
+            CourseworkComponent key = assessmentResult.getKey();
+            if ( !(key instanceof MainComponent) ){
+                continue;
             }
 
-            return;
-
+            if ((!key.getComponentName().equals("Exam")) && ((MainComponent) key).getSubComponents().size() == 0) {
+                availableChoices.add(key.getComponentName());
+                weights.add((double) key.getComponentWeight());
+                isMainAss.add(true);
+            } else {
+                for (SubComponent subComponent : ((MainComponent) key).getSubComponents()) {
+                    availableChoices.add(key.getComponentName() + "-" + subComponent.getComponentName());
+                    weights.add((double) key.getComponentWeight() * (double) subComponent.getComponentWeight() / 100d);
+                    isMainAss.add(false);
+                }
+            }
         }
 
-        System.out.println("This student haven't registered " + courseID);
+        for (int i = 0; i < availableChoices.size(); i++) {
+            System.out.println((i + 1) + ". " + availableChoices.get(i) + " Weight in Total: " + weights.get(i) + "%");
+        }
+        System.out.println((availableChoices.size() + 1) + ". Quit");
+
+        int choice;
+        System.out.println("Enter your choice");
+        choice = scanner.nextInt();
+        scanner.nextLine();
+
+        while (choice > (availableChoices.size() + 1) || choice < 0) {
+            System.out.println("Please enter choice between " + 0 + "~" + (availableChoices.size() + 1));
+            System.out.println("Enter your choice");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+        if (choice == (availableChoices.size() + 1)) {
+            return;
+        }
+
+        double assessmentMark;
+        System.out.println("Enter the mark for this assessment:");
+        assessmentMark = scanner.nextDouble();
+        scanner.nextLine();
+        while (assessmentMark > 100 || assessmentMark < 0) {
+            System.out.println("Please enter mark in range 0 ~ 100.");
+            assessmentMark = scanner.nextDouble();
+            scanner.nextLine();
+        }
+
+        if (isMainAss.get(choice - 1)) {
+            // This is a stand alone main assessment
+            mark.setMainCourseWorkMarks(availableChoices.get(choice - 1), assessmentMark);
+        } else {
+            mark.setSubCourseWorkMarks(availableChoices.get(choice - 1).split("-")[1], assessmentMark);
+        }
 
     }
 
