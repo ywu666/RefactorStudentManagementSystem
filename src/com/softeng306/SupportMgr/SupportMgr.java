45cb6a4262ae1881f4fb25fced247e0495394211
Yujia Wu <ywu660@aucklanduni.ac.nz>
2020-10-23 13:55:21 +0800

    Refactor the HelpInfo by moving some methods using in multiple class to the SupportMgr class.

--- src/com/softeng306/SupportMgr/SupportMgr.java
+++ src/com/softeng306/SupportMgr/SupportMgr.java
@@ -1,6 +1,8 @@
 package com.softeng306.SupportMgr;
 
-import com.softeng306.HelpInfoMgr;
+import com.softeng306.Enum.Department;
+
+import java.util.*;
 
 public abstract class SupportMgr {
 
@@ -12,10 +14,26 @@ public abstract class SupportMgr {
      * @return boolean indicates whether the inputted department is valid.
      */
     public static boolean checkDepartmentValidation(String department){
-        if(HelpInfoMgr.getAllDepartment().contains(department)){
+        if(getAllDepartment().contains(department)){
             return true;
         }
         System.out.println("The department is invalid. Please re-enter.");
         return false;
+    }
+
+    /**
+     * Gets all the departments as an array list.
+     *
+     * @return an array list of all the departments.
+     */
+    public static List<String> getAllDepartment() {
+        Set<Department> departmentEnumSet = EnumSet.allOf(Department.class);
+        List<String> departmentStringList = new ArrayList<String>(0);
+        Iterator iter = departmentEnumSet.iterator();
+        while (iter.hasNext()) {
+            departmentStringList.add(iter.next().toString());
+        }
+        return departmentStringList;
+
     }
 }
