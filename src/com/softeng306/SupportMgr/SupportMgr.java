edbfa5c707df5848e1983db3833727b7236c42c1
Yujia Wu <ywu660@aucklanduni.ac.nz>
2020-10-23 13:17:11 +0800

    Create supportMgr interface and Initialise corresponding support managers.

---
+++ src/com/softeng306/SupportMgr/SupportMgr.java
@@ -0,0 +1,9 @@
+package com.softeng306.SupportMgr;
+
+public interface SupportMgr {
+    void checkExist();
+
+    void checkValidation();
+
+    void checkValidInput();
+}
---
+++ src/com/softeng306/SupportMgr/SupportStudentMgr.java
@@ -0,0 +1,18 @@
+package com.softeng306.SupportMgr;
+
+public class SupportStudentMgr implements SupportMgr{
+    @Override
+    public void checkExist() {
+
+    }
+
+    @Override
+    public void checkValidation() {
+
+    }
+
+    @Override
+    public void checkValidInput() {
+
+    }
+}
